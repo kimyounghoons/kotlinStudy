f1d00831ca9641166b2f41a43c3a196db242630f
kimyounghoons <kimyh8496@gmail.com>
2018-04-03 12:57:21 +0900

    data class

--- KotlinStudy/app/src/main/java/com/study/kotlin/kotlinstudy/MainActivity.kt
+++ KotlinStudy/app/src/main/java/com/study/kotlin/kotlinstudy/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.study.kotlin.kotlinstudy
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.study.kotlin.kotlinstudy.models.Member
+import com.study.kotlin.kotlinstudy.models.Person
 
 class MainActivity : AppCompatActivity() {
-
+    var person: Person = Person("kim", 28)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("MainActivity", person.name + "\n" + "person age : " + person.age)
+
+        person.name = person.name
+        person.age = person.age
+        Log.d("MainActivity", person.name + "\n" + "person age : " + person.age)
+
+        val mem = Member("young", 1)
+        Log.d("MainActivity", mem.name + "\n" + "mem number : " + mem.num)
+
+        mem.name = "222"
+        Log.d("MainActivity", mem.name + "\n" + "mem number : " + mem.num)
+
     }
+
+    data class Mem(var name: String, var num: Int)
 }
---
+++ KotlinStudy/app/src/main/java/com/study/kotlin/kotlinstudy/models/Member.kt
@@ -0,0 +1,6 @@
+package com.study.kotlin.kotlinstudy.models
+
+/**
+ * Created by kimyounghoon on 2018-04-03.
+ */
+data class Member(var name : String , var num : Int)
